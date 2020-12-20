72fe5b18685f4cbb3e345b05c95cbefabcd30a4c
Andy Lustig <alustig3@gmail.com>
2020-12-20 17:52:06 -0500

    New firmware features
    - breathes red when connecting at startup
    - button presses pulse green LED
    - double click knob to turn off

--- software/src/main.cpp
+++ software/src/main.cpp
@@ -1,12 +1,11 @@
 #include <Arduino.h>
 #include <BleKeyboard.h>
 
-BleKeyboard bleKeyboard("Zoom Pad_new","Lustig Labs");
+BleKeyboard bleKeyboard("Hello Andy","Lustig Labs");
 
-// const byte encoder_BTN = 25;
 const byte latch_BTN = 25;
 const byte encoderPin1 = 22;
-const byte encoderPin2 = 21;
+const byte encoderPin2 = 23;
 volatile int lastEncoded = 0;
 int tempNow=0,tempOld = 0;
 const byte knob_sensitivity = 2;
@@ -16,19 +15,17 @@ const int redPWM = 0;
 const int greenPWM = 1;
 const int freq = 500; // 5000 Khz produced a hum at 50% duty cycle, so brought this down
 const int resolution = 8;
-int level = 0;
 const byte btn_4 = 16;
 const byte btn_3 = 17;
 const byte btn_2 = 18;
 const byte btn_1 = 19;
-const int hold_time = 2000;
-const byte max_bright = 75;
+const int hold_time = 850;
+byte knob_brightness = 10;
 
 bool isMAC = true;
-const byte hack = 14;
 
 unsigned long last_active;
-const long idle_limit = 60 * 1000*60; // turn off after 60 minutes of inactivity
+const long idle_limit = 20 * 1000*60; // turn off after 20 minutes of inactivity
 
 void updateEncoder(){
   int MSB = digitalRead(encoderPin1); //MSB = most significant bit
@@ -55,9 +52,13 @@ void shortcut(char ltr){
   else{
     bleKeyboard.press(KEY_LEFT_ALT);
   }
-    bleKeyboard.press(ltr);
-    delay(250);
-    bleKeyboard.releaseAll();
+  bleKeyboard.press(ltr);
+  bleKeyboard.releaseAll();
+  for (int i = 255; i >knob_brightness; i--){
+    ledcWrite(greenPWM,i);
+    delay(1);
+  }
+  delay(50);
 }
 
 void shutdown(){
@@ -71,90 +72,122 @@ void reset_inactive_clock(){
   last_active = millis();
 }
 
+bool double_click( int btn_pin){
+  while(!digitalRead(btn_pin)){
+    delay(2); //in first press
+  }
+  int second_press_timer = 0;
+  bool double_clicked = false;
+  while(second_press_timer < 200){
+    delay(1);
+    second_press_timer++;
+    if (!digitalRead(btn_pin)){
+      Serial.println("Double click!");
+      double_clicked = true;
+      break;
+    }
+  }
+  Serial.println(second_press_timer);
+  return double_clicked;
+}
+
+void connecting(){
+  ledcWrite(greenPWM,0);
+  bool going_up = true;
+  byte red_brightness = knob_brightness;
+  while(!bleKeyboard.isConnected()){
+    delay(2);
+    if (!digitalRead(latch_BTN)){
+      if(double_click(latch_BTN)){
+        shutdown();
+      }
+    }
+    if (going_up){
+      if (red_brightness<255){
+        red_brightness++;
+        ledcWrite(redPWM,red_brightness);
+      }
+      else{
+        going_up = false;
+      }
+    }
+    else{
+      if (red_brightness>0){
+        red_brightness--;
+        ledcWrite(redPWM,red_brightness);
+      }
+      else{
+        going_up = true;
+      }
+    }
+  }
+  ledcWrite(redPWM,0);
+  ledcWrite(greenPWM,knob_brightness);
+}
+
 void setup() {
-  pinMode(hack, OUTPUT);
-  digitalWrite(hack,HIGH);
   pinMode(latch_BTN, INPUT_PULLUP);
   ledcSetup(redPWM, freq, resolution);
+  ledcWrite(redPWM,knob_brightness);
   ledcSetup(greenPWM, freq, resolution);
-  ledcWrite(greenPWM,level);
   ledcAttachPin(red_LED, redPWM);
   ledcAttachPin(green_LED, greenPWM);
   pinMode(btn_1,INPUT_PULLUP);
   pinMode(btn_2,INPUT_PULLUP);
   pinMode(btn_4,INPUT_PULLUP);
   pinMode(btn_3,INPUT_PULLUP);
-  Serial.begin(115200);
-  encoderSetup();
   bleKeyboard.begin();
+  connecting();
+  encoderSetup();
+  Serial.begin(115200);
   reset_inactive_clock();
+  while(!digitalRead(latch_BTN)){
+    delay(1);
+  }
 }
 
 void loop() {
-  ledcWrite(redPWM,level);
-  ledcWrite(greenPWM,max_bright-level);
+  while(!bleKeyboard.isConnected()){
+    connecting();
+  }
   if (tempNow-tempOld>knob_sensitivity){ //CCW knob turn
     Serial.println("CW");
     bleKeyboard.write(KEY_MEDIA_VOLUME_UP);
-    level+=10;
-    if (level>max_bright){
-      level = max_bright;
-    }
     tempNow = tempOld;
-    digitalWrite(red_LED,HIGH);
     reset_inactive_clock();
   }
   else if (tempNow-tempOld<-knob_sensitivity){ //CW knob turn
     Serial.println("CCW");
-    level-=10;
-    if (level<0){
-      level = 0;
-    }
     bleKeyboard.write(KEY_MEDIA_VOLUME_DOWN);
     tempNow = tempOld;
     reset_inactive_clock();
   }
   else if (!digitalRead(latch_BTN)){
-    Serial.println("Encoder Press!");
-    unsigned long presstime = millis();
-    while(!digitalRead(latch_BTN)){
-      if(millis() - presstime > hold_time){
-        shutdown();
-      }
+    if(double_click(latch_BTN)){
+      shutdown();
     }
-    if( millis() - presstime < 200){
-      delay(200);
-    }
-    shortcut('H');
     reset_inactive_clock();
   }
   else if (!digitalRead((btn_1))){
-    Serial.println("YEllow Pressed!");
-    delay(200);
     shortcut('A');
     reset_inactive_clock();
   }
   else if (!digitalRead((btn_2))){
-    Serial.println("Green Pressed!");
-    delay(200);
+    Serial.println("GREEN Pressed!");
     shortcut('V');
     reset_inactive_clock();
   }
   else if (!digitalRead((btn_3))){
     Serial.println("BLUE Pressed!");
-    delay(200);
     shortcut('S');
     reset_inactive_clock();
   }
   else if (!digitalRead((btn_4))){
     Serial.println("Red Pressed!");
-    delay(200);
     shortcut('W');
     reset_inactive_clock();
   }
   else if (millis() - last_active > idle_limit){
     shutdown();
   }
-
 }
-
